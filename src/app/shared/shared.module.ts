d1cb0e88aad679e98a9d37676fef03f2f8c5956a
Vamsi G <spgaddipati@gmail.com>
2023-11-09 19:47:58 -0500

    Add files via upload

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { AppRoutingModule } from '../app-routing.module';
 import { StarComponent } from './star.component';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-
 @NgModule({
   imports: [
     CommonModule,
-    AppRoutingModule,
+    RouterModule,
     MatInputModule,
     MatIconModule,
     MatButtonModule,
